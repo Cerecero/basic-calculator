d4f5f6b4659a2ef5db20d8d18b44cffbca8fd07a
Cerecero <ale.rey.cer@outlook.com>
2023-01-03 16:32:41 -0600

    limited the input for only numbers and a certain length

--- script/script.js
+++ script/script.js
@@ -1,12 +1,32 @@
 // let test = document.getElementById("seven").value;
 
 // console.log(parseInt(test));
+// const textInput = document.getElementById("text");
+
+// textInput.addEventListener('input', (event) => {
+//     const value = event.target.value;
+//     console.log(value);
+//     const accentedLetters =/^[0-9]+$/g;
+    
+//     if (accentedLetters.test(!value)){ /*Checks if there are accented letters, capital letters or numbers*/
+//         event.preventDefault();
+//         textInput.value = value.replace(accentedLetters, ""); /*Removes the accented letters, capital letters and numbers*/
+//     }
+// });
+const inputText = document.getElementById('text');
+
+inputText.addEventListener('input', function(event) {
+    if (this.value.length > 10) {
+        event.preventDefault();
+        this.value = this.value.slice(0, 10);
+    }
+});
 
 const buttonClick = document.querySelectorAll(".number");
 
 buttonClick.forEach(function(button) {
     button.addEventListener('click', function(event) {
       // Your event handling code goes here
-      console.log(event.target.value);
+        console.log(event.target.value);
     });
-  });
+});
